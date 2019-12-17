280f895f8007e5fb59180329a5842f49f567db23
embee0 <micbre@gmail.com>
2019-12-17 17:08:34 +0100

    Aufgaben zu HOFs

--- src/AufgabenHOFs.kt
+++ src/AufgabenHOFs.kt
@@ -4,18 +4,6 @@ import java.time.format.DateTimeFormatter
 
 data class Person(val kundenNr: Int, val vorname: String, val nachname: String, val geburtsdatum: LocalDate, val geschlecht: Char)
 
-val persons: List<Person> = readPersonFile(File("personen.csv"))
-
-fun main() {
-
-
-    for (person in persons) {
-        println(person)
-    }
-
-}
-
-
 fun readPersonFile(csv: File): List<Person> {
     val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
     val lines = csv.readLines().map { it.split(";") }
@@ -24,5 +12,32 @@ fun readPersonFile(csv: File): List<Person> {
         Person(nr.toInt(), vn, nn, LocalDate.parse(geb, formatter), mw[0])
     }
 }
+
+val persons: List<Person> = readPersonFile(File("personen.csv"))
+
+
+fun main() {
+    aufgabe0()
+}
+
+fun aufgabe0() {
+    println("Gib alle Personen in der Datenbank aus:")
+    for (person in persons) {
+        println(person)
+    }
+
+    // Stelle nunden Cursor oben auf das Wort "for" und drücke Alt-Enter.
+    // Welche Optionen den Code zu verändern schlägt dir IntelliJ vor?
+    // Versuche die folgenden Varianten zu erzeugen:
+
+    persons.forEach { println(it) }
+
+    persons.forEach(::println)
+
+    // Welche Lösung findest du selbst am besten?
+}
+
+
+
 
 
